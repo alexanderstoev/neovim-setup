792e902bffc9ede972cf8b32e4319791785a6c32
Alex Stoev <alex.stoev@gmail.com>
2024-12-21 20:40:39 +0200

    <leader>e will now smartly toggle between the Neotree and the buffers

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,3 +1,49 @@
+local function smart_toggle_neotree() 
+
+	local api = vim.api
+	local neotree_buffer_types = { "filesystem", "buffers", "git_status" }
+	local is_neotree_visible = false
+	local neotree_win_id = nil
+
+
+	-- Check if Neo-tree is visible
+	for _, win_id in ipairs(api.nvim_list_wins()) do
+		local bufname = api.nvim_buf_get_name(api.nvim_win_get_buf(win_id))
+		for _, type in ipairs(neotree_buffer_types) do
+			if bufname:match(type) then
+				is_neotree_visible = true
+				neotree_win_id = win_id
+				break
+			end
+		end
+		if is_neotree_visible then
+			break
+		end
+	end
+
+	local current_win = api.nvim_get_current_win()
+
+	if is_neotree_visible then
+		-- If cursor is in Neo-tree, move to the visible buffer
+		if current_win == neotree_win_id then
+			for _, win_id in ipairs(api.nvim_list_wins()) do
+				if win_id ~= neotree_win_id then
+					api.nvim_set_current_win(win_id)
+					return
+				end
+			end
+		else
+			-- Focus Neo-tree if in a file buffer
+			api.nvim_set_current_win(neotree_win_id)
+		end
+	else
+		-- Show Neo-tree if not visible and focus it
+		vim.cmd("Neotree show")
+	end
+
+end;
+
+
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
@@ -18,7 +64,7 @@ return {
 		})
 
 		-- keymaps for Neo-tree
-		vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>", { desc = "Toggle Neo-tree" })
+		vim.keymap.set("n", "<leader>e", smart_toggle_neotree, { desc = "Toggle Neo-tree" })
 	end
 
 }
