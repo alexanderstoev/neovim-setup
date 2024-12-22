acb396fee21cc50d6b955e4abda3f815a6481a9d
Alex Stoev <alex.stoev@gmail.com>
2024-12-22 16:58:45 +0200

    added snaks to fix bufferline close command

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,21 +1,25 @@
 return {
 	'akinsho/bufferline.nvim', 
+	event = "VeryLazy",
 	version = "*", 
 	dependencies = 'nvim-tree/nvim-web-devicons',
 	config = function() 
 		require("bufferline").setup({
 			options = {
-				numbers = "buffer_id",
-				separator_style =  "slope", 
+				-- stylua: ignore
+				close_command = function(n) Snacks.bufdelete(n) end,
+				-- stylua: ignore
+				right_mouse_command = function(n) Snacks.bufdelete(n) end,
+				diagnostics = "nvim_lsp",
+				always_show_bufferline = false,
 				offsets = {
 					{
 						filetype = "neo-tree",
-						text = "",
-						separator = true,
-						text_align = "left"
-					}
+						text = "Neo-tree",
+						highlight = "Directory",
+						text_align = "left",
+					},
 				},
-
 			}
 		})
 	end;
