96d5c0a60ba4d5d6a2c9018f37975e4d806052be
Alex Stoev <alex.stoev@gmail.com>
2025-01-19 15:49:36 +0200

    telescope config from kickstart

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,3 @@
--- telescope
-local telescope_builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', telescope_builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', telescope_builtin.help_tags, { desc = 'Telescope help tags' })
 
 -- Keybinds to make split navigation easier.
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
