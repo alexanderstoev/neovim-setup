59a514818c63366fa1762925ab2543f17a519782
Alex Stoev <alex.stoev@gmail.com>
2025-01-19 16:03:58 +0200

    dsable netrw

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -46,6 +46,21 @@ require("lazy").setup({
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        -- "gzip",
+        -- "matchit",
+        -- "matchparen",
+        "netrwPlugin",
+        -- "tarPlugin",
+        -- "tohtml",
+        -- "tutor",
+        -- "zipPlugin",
+      },
+    },
+  },
 })
 
 require("config.keymaps")
